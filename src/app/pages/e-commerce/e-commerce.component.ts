ae4733b8ff6565fbb5730d0003a5a96c7a1683bc
Nikhil Batra <NIBA@saxobank.com>
2019-11-26 15:21:31 +0530

    first commit

---
+++ src/app/pages/e-commerce/e-commerce.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-ecommerce',
+  templateUrl: './e-commerce.component.html',
+})
+export class ECommerceComponent {
+  
+  sliders = [{
+    "imagePath": "assets/images/slider1.jpg",
+    "label": "First slide label",
+    "text": "This is the first slide image"
+},
+{
+    "imagePath": "assets/images/slider2.jpg",
+    "label": "Second slide label",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+},
+{
+    "imagePath": "assets/images/slider3.jpg",
+    "label": "Third slide label",
+    "text": "Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+}];
+}
---
+++ src/app/pages/e-commerce/slider/slider.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss']
+})
+export class SliderComponent implements OnInit {
+  @Input() sliders: string;
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.sliders)
+  }
+
+}
